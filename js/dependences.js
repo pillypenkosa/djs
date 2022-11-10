b40a4e22cb1842e96a0cc3115ac48638039b60c2
pillypenkosa <pillypenkosa@gmail.com>
2022-11-10 20:37:04 +0200

    djs

---
+++ js/dependences.js
@@ -0,0 +1,32 @@
+const dependences = {
+
+	components: [
+	
+		'btn',
+
+		'any',
+
+		'header',
+		'aside',
+		'middle',
+	],
+
+	listGIT: [ 		// https://pillypenkosa.github.io/database/list/list. ${ k } .js
+
+		'surnames',
+	],
+
+	listApp: [ 		// list/list. ${ k } .js
+
+		'any',
+	],
+
+};
+
+
+
+
+
+
+
+
