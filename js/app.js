4aea1c42b848a79f2817bf3b0bfac11a3a7eb0d7
pillypenkosa <pillypenkosa@gmail.com>
2023-09-13 23:16:09 +0300

    DJS 2023-09-13 23:10

--- js/app.js
+++ js/app.js
@@ -35,6 +35,49 @@ function addErr( txt = '' ) {
 
 function isErr() {
 
+	let ok = 'isErr() - помилок не зафіксовано )))';
+	let err = 'isErr() - виявлені помилки:';
+
+
+
+	if ( errConsole.length ) {
+
+
+		//cns( 'Бля буду, помилки хуярять... ', 'шось', 'err' );
+
+		console.log( err );
+
+		errConsole.forEach( k => {
+			console.log( k );
+		});
+
+	} else 
+		console.log( ok );
+		//cns( 'ok', 'Помилок не вижу... ', );
+
+
+	errConsole = [];	
+}
+
+
+//function cns( txt, obj, type = 'ok' ) { 	// t - type: текст `ok`, `err`, `warning`, 	// txt - текстова назва змінної 		// obj - змінна з конкретним змістом 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function isErr222() {
+
 	let ok = `\x1b[32m ok isErr() - помилок не зафіксовано )))`;
 	let err = `\x1b[31m err isErr() виявлені помилки:`;
 
@@ -51,11 +94,6 @@ function isErr() {
 
 	errConsole = [];	
 }
-
-
-
-
-
 
 
 
@@ -162,8 +200,230 @@ function big1Letter222( txt ) {
 
 
 
+// виведення інформвції в консоль
+function cns( type, txt, obj ) { 	// t - type: текст `ok`, `err`, `warning`, 	// txt - текстова назва змінної 		// obj - змінна з конкретним змістом 
+
+	let bgFrom 			= '';
+	let bgTo 			= '';
+	let borderRadiusTL 	= 10; 	// L - left, R - right, T - top, B - bottom
+	let borderRadiusTR 	= 10; 	// L - left, R - right, T - top, B - bottom
+	let borderRadiusBL 	= 10; 	// L - left, R - right, T - top, B - bottom
+	let borderRadiusBR 	= 10; 	// L - left, R - right, T - top, B - bottom
+	let css 			= '';
+
+	let outContent 		= '';
 
 
+	if ( type == 'ok' ) {
+		bgFrom 	= '0f0';
+		bgTo 	= '050';
+	}
+
+	if ( type == 'err' ) {
+		bgFrom 	= 'f00';
+		bgTo 	= '500';
+	}
+
+	if ( type == 'warning' ) {
+		bgFrom 	= 'fc0';
+		bgTo 	= 'f90';
+	}
+
+	if ( type == 'var' ) {
+		bgFrom 	= '777';
+		bgTo 	= '111';
+		borderRadiusTR 	= 0; 	// L - left, R - right, T - top, B - bottom
+		borderRadiusBR 	= 0; 	// L - left, R - right, T - top, B - bottom
+	}
+
+
+	css = `
+		padding: 0 10px; 
+		border-top-left-radius: ${ borderRadiusTL }px; 
+		border-top-right-radius: ${ borderRadiusTR }px; 
+		border-bottom-left-radius: ${ borderRadiusBL }px; 
+		border-bottom-right-radius: ${ borderRadiusBR }px; 
+		font-size: 10px; 
+		font-family: Courier; 
+		font-weight: bold; 
+		background: linear-gradient( to top, #${ bgFrom }, #${ bgTo } ); 
+		color: #fff; 
+	`;
+
+	outContent = `%c${ type }: ${ txt }` ;
+
+
+
+
+	if ( type == 'ok' ) {
+		bgFrom 	= '0f0';
+		bgTo 	= '050';
+
+		console.log( `%cOk! ${ txt }`, css );
+	}
+
+	if ( type == 'err' ) {
+		bgFrom 	= 'f00';
+		bgTo 	= '500';
+
+		console.log( `%cПомилка! ${ txt }`, css );
+	}
+
+	if ( type == 'warning' ) {
+		bgFrom 	= 'fc0';
+		bgTo 	= 'f90';
+
+		console.log( `%cУвага! ${ txt }`, css );
+	}
+
+	if ( type == 'var' ) {
+		bgFrom 	= '777';
+		bgTo 	= '111';
+		borderRadiusTR 	= 0; 	// L - left, R - right, T - top, B - bottom
+		borderRadiusBR 	= 0; 	// L - left, R - right, T - top, B - bottom
+
+		console.log( `%cЗмінна ${ txt }`, css );
+	}
+
+
+
+
+	//if ( obj !== undefined ) 
+		console.log( obj );
+
+
+
+
+
+	return;
+
+
+	console.log( outContent, css );
+
+	//console.log( obj );
+
+
+
+
+
+
+
+
+	let arrDjs1 = {
+
+
+		aaa: { id: 1, key: '123', val: 'qwer', },
+		bbb: { id: 2, key: '456', val: 'asdf', },
+		ccc: { id: 3, key: '789', val: 'zxcv', },
+
+	} ;
+
+
+
+
+
+
+
+
+
+
+	console.groupCollapsed ( 'шо-то у Ашота 1' );
+
+		console.log( 'Какая-то жопа 11...' );
+		console.warn( 'Какая-то жопа 12...' );
+		console.error( 'Какая-то жопа 13...' );
+
+
+		//console.groupEnd();
+
+
+		console.groupCollapsed ( 'шо-то у Ашота 2' );
+
+			console.log( 'Какая-то жопа 21...' );
+			console.log( 'Какая-то жопа 22...' );
+			console.log( 'Какая-то жопа 23...' );
+
+
+		console.groupEnd();
+
+		console.log( 'просто жопа ...' );
+
+	console.groupEnd();
+
+	console.log( 'просто жопа 2...' );
+
+
+
+
+	//let css = "font-size: 20px; font-family: Courier; font-weight: bold; background: linear-gradient( to top, #0f0, #050 ); color: #fff; padding: 0 50px; border-radius: 10px;";
+
+
+	console.log( '%cЛевый текст... меняй подушки...', css );
+
+
+
+	console.warn( 'Name is not provided...' );
+	console.error( 'Какая-то памилка...' );
+
+	console.info( 555 );
+
+
+
+
+
+
+	let color = '\x1b[32m';
+
+
+
+
+	//console.clear();
+
+
+
+
+	console.trace();
+
+
+	let fruits = [ 
+		[ `&#91;Apple`, 'Orange1', 'Banana1' ], 
+		[ `&#91;Apple`, 'Orange2', 'Banana2' ], 
+		[ `&#91;Apple`, 'Orange3', 'Banana3' ], 
+		[ `&#91;Apple`, 'Orange4', 'Banana4' ], 
+		[ `&#91;Apple`, 'Orange5', 'Banana5' ], 
+	];
+
+
+
+
+
+
+
+
+
+	console.table( arrDjs1 );
+
+
+
+
+
+
+
+	//console.log( "%cExample %s", css, 'all code runs happy' );
+	
+
+	console.log( '%cExample all code runs %shappy', css, 'after555' );
+
+
+
+
+
+	console.log( color + k + ':' );
+	console.log( v );
+
+
+
+}
 
 
 
--- js/start.js
+++ js/start.js
@@ -33,4 +33,87 @@ window.onload = function() {
 
 
 
+let txt = 'https://djs.com.ua/url?q=qwerty&win=page1#anyhash';
+
+
+
+let url = new URL( txt );
+
+//console.log( txt );
+
+
+let aaa = null;
+
+cns( 'ok', 'Все зашибись...', );
+cns( 'err', 'Якась дічь...', aaa );
+cns( 'warning', 'Пеньок' );
+cns( 'var', 'Змінна `url`:', url );
+cns( 'var', 'Змінна `txt`:', txt );
+
+
+
+
+//cns( 'ok', txt );
+
+
+
+
+
+//cns( 'Помилка', txt, 'err' );
+//cns( 'Увага', txt, 'warning' );
+//cns( 'Змінна', txt, 'var' );
+
+
+
+
+
+
+
+
+/*
+
+
+console.log( '\x1b[32m url:' );
+console.log( url );
+
+
+console.log( url.protocol );
+console.log( url.host );
+console.log( url.pathname );
+
+
+console.log( '\x1b[32m searchParams:' );
+console.log( url.searchParams );
+
+
+console.log( '\x1b[32m searchParams.get( q ):' );
+console.log( url.searchParams.get( 'q' ));
+
+
+
+console.log( '\x1b[32m searchParams.get( win ):');
+console.log( url.searchParams.get( 'win' ));
+
+
+console.log( '\x1b[32m hash:' );
+console.log( url.hash );
+
+*/
+
+
+
+
+/*
+		let name = 'html';
+		let ok = `\x1b[32m ok ${ this.name }.${ name }() `;
+		let err = `\x1b[31m err ${ this.name }.${ name }() `;
+
+
+*/
+
+
+
+
+
+
 
